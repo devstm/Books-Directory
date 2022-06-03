b07edf8fcf097d9a32a904cf0c5f9f7a8897afcc
unknown <stm1998@hotmail.com>
2022-06-03 12:11:53 +0300

    create modle type

--- server/Models/Author.ts
+++ server/Models/Author.ts
@@ -1,8 +1,18 @@
 import { Table, Column, Model,  DataType, AutoIncrement, PrimaryKey } from 'sequelize-typescript'
+import { Optional } from 'sequelize'
 const {STRING, INTEGER} = DataType;
 
+type Author = {
+  id: number;
+  name: string;
+  email: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+interface AuthorsModel extends Optional<Author, 'id'> {}
+
 @Table
-class authors extends Model {
+class authors extends Model<Author, AuthorsModel> {
   @Column(STRING)
     name: string| undefined;
 
